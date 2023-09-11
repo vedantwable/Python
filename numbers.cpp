7f344110f247d336e5b40618af93f8e9b60b1b3c
vedantwable <142864313+vedantwable@users.noreply.github.com>
2023-09-11 14:55:14 +0530

    Add files via upload

---
+++ numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+   // loop from 1 to 10 and print each number
+  for (int i=1; i<=10; ++i) {
+      std::cout << i << "";
+  }
+  std::cout << std::endl; // print the newline after the numbers
+  return 0;
+  }
